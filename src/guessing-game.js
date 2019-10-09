6c6ef2b97aa7260000b09f3c2296ca3c21d1c5c3
Hanna Dozhyna <annadozhyna@gmail.com>
2019-10-09 12:38:48 +0300

    add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.first = 0;
+    this.last = 0;
+    this.middle = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.first = min;
+    this.last = max;
+  }
 
-    }
+  guess() {
+    this.middle = Math.round(this.first + (this.last - this.first) / 2);
+    return this.middle;
+  }
 
-    guess() {
+  lower() {
+    this.last = this.middle;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.first = this.middle;
+  }
 }
 
 module.exports = GuessingGame;
